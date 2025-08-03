61b407088a7c60040330a73f988a4846e099fc3c
anushka-das <anushkadas942@gmail.com>
2025-08-03 23:55:21 +0545

    I have added the slider

--- script/script.js
+++ script/script.js
@@ -1,26 +1,53 @@
-const slides = document.querySelectorAll(".slide")
-var counter = 0;
-slides.forEach(
-  (slide,index)=>{
-    slide.style.left = `${index * 100}%`
-  }
-)
+const slides = document.querySelectorAll(".slide");
+let counter = 0;
 
-const goPrev =()=>{
-  counter--
-  slideImage()
-}
+// Dot container
+const dotContainer = document.querySelector(".dot-container");
 
-const goNext =()=>{
-  counter++
-  slideImage()
-}
+slides.forEach(() => {
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+  dotContainer.appendChild(dot);
+});
 
-const slideImage = ()=>{
-  slides.forEach(
-    (slide)=>{
-      slide.style.transform =`translateX(-${counter *100}%)`
-    }
-  )
+const dots = document.querySelectorAll(".dot");
 
-}
+const updateDots = () => {
+  dots.forEach(dot => dot.classList.remove("active"));
+  dots[counter].classList.add("active");
+};
+
+
+// Set initial positions
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slideImage = () => {
+  if (counter < 0) counter = slides.length - 1;
+  if (counter >= slides.length) counter = 0;
+
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+
+  updateDots();
+};
+
+const goPrev = () => {
+  counter--;
+  slideImage();
+};
+
+const goNext = () => {
+  counter++;
+  slideImage();
+};
+
+// Auto Slide every 4 seconds
+setInterval(() => {
+  counter++;
+  slideImage();
+}, 4000);
+
+updateDots();
